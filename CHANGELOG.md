c97a8d5119eb05efa5509fbca3baacc953da4150
Gabriel Csapo <gabecsapo@gmail.com>
2017-11-21 10:46:29 -0800

    0.0.1 - adds build command can be run as build or build --directory=./some/directory - adds markdown parsing for inlineCode - ensures inner table elements are parsed correctly (if inner table elements contain nested markdown) - adds slug attribute to post object (this is the name of the html file to be referenced when linking) - has the ability to generate posts and root level templates

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,11 @@
+# 0.0.1 (11/21/2017)
+
+- adds `build` command can be run as `build` or `build --directory=./some/directory`
+- adds markdown parsing for `inlineCode`
+- ensures inner table elements are parsed correctly (if inner table elements contain nested markdown)
+- adds slug attribute to post object (this is the name of the html file to be referenced when linking)
+- has the ability to generate posts and root level templates
+
 # 0.0.0 (11/17/2017)
 
 - idea started
--- bin/sweeney.js
+++ bin/sweeney.js
@@ -1,10 +1,30 @@
 #!/usr/bin/env node
+const path = require('path');
+const qs = require('qs');
+
+const generate = require('../lib/generate');
 
 const args = process.argv.slice(2);
 
+const options = {};
+args.filter((a) => a.indexOf('--') > -1).forEach((a) => Object.assign(options, qs.parse(a)));
+
 switch(args[0]) {
   case 'build':
-    console.log('not implemented :('); // eslint-disable-line
+    (async function() {
+      try {
+        const directory = path.resolve(process.cwd(), options['--directory'] || './');
+        const config = require(path.resolve(directory, 'sweeney.js'));
+
+        await generate(directory, config);
+        console.log(`site built at ${path.resolve(process.cwd(), options['--directory'] || './')}`); // eslint-disable-line
+      } catch(ex) {
+        console.log(`uhoh something happened \n ${ex.toString()}`); // eslint-disable-line
+      }
+    }());
+  break;
+  case 'serve':
+    
   break;
   default:
     console.log(`sorry the command ${args[0]} is not supported`); // eslint-disable-line
